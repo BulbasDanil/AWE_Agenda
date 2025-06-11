51a610e7240993540a44aa93ed2f295f69a03311
Danyl Bulbas <bulbas_danil@protonmail.com>
2025-06-10 23:08:30 -0400

    feat: agenda command support

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,12 @@
 import { TpaServer, TpaSession, ViewType } from '@augmentos/sdk';
+import * as ical from 'ical';
 
 
 const PACKAGE_NAME = process.env.PACKAGE_NAME ?? (() => { throw new Error('PACKAGE_NAME is not set in .env file'); })();
 const AUGMENTOS_API_KEY = process.env.AUGMENTOS_API_KEY ?? (() => { throw new Error('AUGMENTOS_API_KEY is not set in .env file'); })();
 const PORT = parseInt(process.env.PORT || '3000');
+
+const ICAL = process.env.ICAL ?? (() => { throw new Error('ICAL is not set in .env file'); })();
 
 class ExampleAugmentOSApp extends TpaServer {
 
@@ -15,6 +18,17 @@ class ExampleAugmentOSApp extends TpaServer {
     });
   }
 
+  protected getNextEvent(icsData: string) {
+    const events = ical.parseICS(icsData);
+    const now = new Date();
+  
+    const upcomingEvents = Object.values(events)
+      .filter(e => e.type === 'VEVENT' && e.start && e.start > now)
+      .sort((a, b) => (a.start.getTime() - b.start.getTime()));
+  
+    return upcomingEvents[0]; // the next event
+  }
+
   protected async onSession(session: TpaSession, sessionId: string, userId: string): Promise<void> {
 
     // Show welcome message
@@ -24,11 +38,19 @@ class ExampleAugmentOSApp extends TpaServer {
     // requires microphone permission to be set in the developer console
     const eventHandlers = [
       session.events.onTranscription((data) => {
-        if (data.isFinal) {
-          session.layouts.showTextWall("You said: " + data.text, {
-            view: ViewType.MAIN,
-            durationMs: 3000
+        if (data.isFinal && data.text?.toLowerCase().includes("agenda")) {
+
+          fetch(ICAL)
+          .then(res => res.text())
+          .then(icsData => {
+            const nextEvent = this.getNextEvent(icsData);
+            session.layouts.showTextWall("Next event: " + nextEvent?.summary + " at " + nextEvent?.start.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' }), {
+              view: ViewType.MAIN,
+              durationMs: 7500
+            });
+            
           });
+
         }
       }),
 
